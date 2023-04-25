9afa85a66031faa71e7c1eb30f52f21f871850a6
Otavio Cartaxo <oacartaxo@gmail.com>
2023-04-25 02:48:44 -0300

    lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    compileOnly("org.projectlombok:lombok:1.18.24")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
