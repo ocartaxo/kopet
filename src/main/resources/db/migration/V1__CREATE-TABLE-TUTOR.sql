c76322573a64a8e5a1bd0e245a4b7ce43a1cf67f
Otavio Cartaxo <oacartaxo@gmail.com>
2023-04-25 02:49:00 -0300

    tabela tutor

---
+++ src/main/resources/db/migration/V1__CREATE-TABLE-TUTOR.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tutor
+(
+    id         SERIAL PRIMARY KEY,
+    email      VARCHAR(50)  NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    name       VARCHAR(50)  NOT NULL,
+    created_on TIMESTAMP(6) NOT NULL,
+
+    phone      VARCHAR(50),
+    about      varchar(500),
+    image      VARCHAR(255),
+
+    /*LOCATION*/
+    address    VARCHAR(50),
+    number     INTEGER,
+    district   VARCHAR(50),
+    city       VARCHAR(50),
+    state      VARCHAR(10),
+    zip_code   VARCHAR(50)
+)
