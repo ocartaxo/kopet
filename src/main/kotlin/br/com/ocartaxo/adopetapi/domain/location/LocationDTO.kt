1985bf4c38b97053d56f47e3195b9cf5627a0964
Otavio Cartaxo <oacartaxo@gmail.com>
2023-04-25 02:49:49 -0300

    localizacao

---
+++ src/main/kotlin/br/com/ocartaxo/adopetapi/domain/location/LocationDTO.kt
@@ -0,0 +1,24 @@
+package br.com.ocartaxo.adopetapi.domain.location
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class LocationDTO(
+    @JsonProperty("endereco")
+    val address: String,
+    @JsonProperty("numero")
+    val number: Int,
+    @JsonProperty("bairro")
+    val district: String,
+    @JsonProperty("cidade")
+    val city: String,
+    @JsonProperty("estado")
+    val state: States,
+    @JsonProperty("cep")
+    val zipCode: String
+) {
+    override fun toString(): String {
+        return "$address, $number - $district, $city - $state, $zipCode"
+    }
+
+
+}
