ee799e530d48c9cdbca6228253c60d20fabceb3f
Otavio Cartaxo <oacartaxo@gmail.com>
2023-04-25 04:00:40 -0300

    pet

---
+++ src/main/kotlin/br/com/ocartaxo/adopetapi/domain/pet/PetSize.kt
@@ -0,0 +1,9 @@
+package br.com.ocartaxo.adopetapi.domain.pet
+
+enum class PetSize {
+    MINI,
+    SMALL,
+    MEDIUM,
+    LARGE,
+    GIANT,
+}
---
+++ src/main/kotlin/br/com/ocartaxo/adopetapi/domain/pet/PetSpecie.kt
@@ -0,0 +1,9 @@
+package br.com.ocartaxo.adopetapi.domain.pet
+
+enum class PetSpecie {
+    CAT,
+    DOG,
+    BIRD,
+    RODENT,
+    MONKEY
+}
