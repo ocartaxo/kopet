0b476cb764c3eb6ec254a9865ae1176e234a4118
Otavio Cartaxo <oacartaxo@gmail.com>
2023-05-21 02:14:01 -0300

    jwt libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,6 +37,10 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+
+    // jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 }
 
 tasks.withType<KotlinCompile> {
