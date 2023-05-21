b337801ec8d1237f04b13bec61e70685baf03fa5
Otavio Cartaxo <oacartaxo@gmail.com>
2023-05-21 01:52:00 -0300

    autentica usuário

---
+++ src/main/kotlin/br/com/ocartaxo/adopetapi/controller/AuthController.kt
@@ -0,0 +1,25 @@
+package br.com.ocartaxo.adopetapi.controller
+
+import br.com.ocartaxo.adopetapi.domain.user.AuthenticationRequest
+import br.com.ocartaxo.adopetapi.domain.user.AuthenticationResponse
+import br.com.ocartaxo.adopetapi.domain.user.AuthenticationService
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/authenticate")
+class AuthController(
+    private val service: AuthenticationService
+) {
+
+    @PostMapping
+    fun login(@RequestBody @Valid bodyRequest: AuthenticationRequest): ResponseEntity<AuthenticationResponse> {
+        return ResponseEntity.ok().body(service.authenticate(bodyRequest))
+    }
+
+
+}
