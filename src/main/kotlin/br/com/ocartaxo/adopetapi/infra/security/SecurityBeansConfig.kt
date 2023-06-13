91aba7ba33df63e45dd83ed1c0c7e98e664dda41
ocartaxo <oacartax@gmail.com>
2023-06-13 16:25:16 -0300

    rename

--- src/main/kotlin/br/com/ocartaxo/adopetapi/infra/security/SecurityBeansConfig.kt
+++ src/main/kotlin/br/com/ocartaxo/adopetapi/infra/security/SecurityBeansConfig.kt
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
-class AppAuthenticationConfig(
+class SecurityBeansConfig(
     private val repository: UsersRepository
 ) {
 
