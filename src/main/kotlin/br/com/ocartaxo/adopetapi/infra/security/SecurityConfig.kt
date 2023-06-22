a368be2d235c17bd53ec9d7be8b98f0fb9402dc5
ocartaxo <oacartax@gmail.com>
2023-06-22 07:01:55 -0300

    endpoints publicos

--- src/main/kotlin/br/com/ocartaxo/adopetapi/infra/security/SecurityConfig.kt
+++ src/main/kotlin/br/com/ocartaxo/adopetapi/infra/security/SecurityConfig.kt
@@ -10,7 +10,6 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
-typealias AuthConfig=AuthenticationConfiguration
 
 @Configuration
 @EnableWebSecurity
@@ -18,17 +17,23 @@ class SecurityConfig(
     private val jwtAuthenticationFilter: JwtAuthenticationFilter
 ) {
 
+    private val allowedURIs = arrayOf(
+        "/v3/api-docs/**",
+        "/swagger-ui.html",
+        "/swagger-ui/**",
+        "/authenticate"
+    )
+
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
 
         http.csrf().disable()
             .sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authorizeHttpRequests { request ->
-                request.requestMatchers(HttpMethod.POST, "/authenticate").permitAll()
+                request.requestMatchers(*allowedURIs).permitAll()
                 request.requestMatchers(HttpMethod.POST, "/abrigos").hasRole("ABRIGO")
                 request.requestMatchers(HttpMethod.POST, "/tutores").hasRole("TUTORES")
-                // Swagger
-                request.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+
 
             }
             .authorizeHttpRequests().anyRequest().authenticated().and()
