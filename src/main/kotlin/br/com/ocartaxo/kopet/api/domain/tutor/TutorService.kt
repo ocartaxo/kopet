28fc40e740f468730e8a05d0170b149c77e9f575
ocartaxo <oacartax@gmail.com>
2024-08-29 20:32:51 -0300

    anotacoes e servico para registrar tutor

--- src/main/kotlin/br/com/ocartaxo/kopet/api/domain/tutor/TutorService.kt
+++ src/main/kotlin/br/com/ocartaxo/kopet/api/domain/tutor/TutorService.kt
@@ -1,6 +1,9 @@
 package br.com.ocartaxo.kopet.api.domain.tutor
 
+import br.com.ocartaxo.kopet.api.domain.user.RegistrationService
+import jakarta.transaction.Transactional
 import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
@@ -8,23 +11,30 @@ import org.springframework.stereotype.Service
 @Service
 class TutorService(
     private val tutorsRepository: TutorsRepository,
+    private val registrationService: RegistrationService
 ) {
 
     private val logger = LoggerFactory.getLogger(TutorService::class.java)
 
-    fun register(request: TutorRequest): TutorResponse {
-        logger.info("Cadastrando o usuário `${request.email}`")
-        val t: Tutor = request.toEntity()
+    @Transactional
+    fun register(tutorRequest: TutorRequest): TutorResponse {
+        logger.info("Cadastrando o usuário `${tutorRequest.email}`")
+
+        val t = tutorRequest.toEntity(registrationService.register(tutorRequest.toRegisterRequest()))
+
         tutorsRepository.save(t)
         return t.toDTO()
     }
 
+    @Cacheable("tutores")
     fun list(pageable: Pageable): Page<TutorSummaryResponse> =
         tutorsRepository.findAll(pageable).map(Tutor::toSummaryResponse)
 
+    @Cacheable("tutor_por_id")
     fun show(id: Int): TutorResponse = tutorsRepository.findById(id)
         .orElseThrow { IllegalArgumentException("Tutor de id $id não está cadastrado na base!") }.toDTO()
 
+    @Transactional
     fun update(request: TutorUpdateRequest): TutorResponse {
         val t: Tutor = tutorsRepository.findById(request.id).orElseThrow()
         t.update(request)
