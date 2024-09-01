8f377b11c6bad65a6be41456f4f586a7530e6a08
ocartaxo <oacartax@gmail.com>
2024-08-31 22:59:33 -0300

    fix: rota de autenticacao whitelist

--- src/main/kotlin/br/com/ocartaxo/kopet/api/infra/security/SecurityConfig.kt
+++ src/main/kotlin/br/com/ocartaxo/kopet/api/infra/security/SecurityConfig.kt
@@ -29,7 +29,7 @@ class SecurityConfig(
     private val POST_WHITELIST = arrayOf(
         "/api/tutores",
         "/api/abrigos",
-        "/api/authenticate"
+        "/api/auth"
     )
 
     @Bean
