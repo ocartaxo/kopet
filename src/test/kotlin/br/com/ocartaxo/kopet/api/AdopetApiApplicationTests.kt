6dfda4808de36d447b77a25425592ffeea39e39c
ocartaxo <oacartaxo@gmail.com>
2024-09-15 01:30:34 -0300

    disable test

--- src/test/kotlin/br/com/ocartaxo/kopet/api/AdopetApiApplicationTests.kt
+++ src/test/kotlin/br/com/ocartaxo/kopet/api/AdopetApiApplicationTests.kt
@@ -1,9 +1,11 @@
 package br.com.ocartaxo.kopet.api
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@Disabled
 class AdopetApiApplicationTests {
 
     @Test
