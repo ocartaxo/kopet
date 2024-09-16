cb8eb4e06106897d51c753057cf0e9d1a0ed5a36
ocartaxo <oacartaxo@gmail.com>
2024-09-15 22:42:59 -0300

    move anotacoes de cache para service

--- src/main/kotlin/br/com/ocartaxo/kopet/api/controller/SheltersController.kt
+++ src/main/kotlin/br/com/ocartaxo/kopet/api/controller/SheltersController.kt
@@ -4,22 +4,22 @@ import br.com.ocartaxo.kopet.api.domain.shelter.ShelterRequest
 import br.com.ocartaxo.kopet.api.domain.shelter.ShelterService
 import br.com.ocartaxo.kopet.api.domain.shelter.ShelterSummaryResponse
 import br.com.ocartaxo.kopet.api.domain.shelter.ShelterUpdateRequest
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.transaction.Transactional
 import jakarta.validation.Valid
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
 
-@RequestMapping("/api/abrigos")
 @RestController
 class SheltersController(private val service: ShelterService) {
 
     @PostMapping
-    @Transactional
     fun register(
         @RequestBody @Valid request: ShelterRequest,
         builder: UriComponentsBuilder
@@ -30,7 +30,6 @@ class SheltersController(private val service: ShelterService) {
     }
 
     @GetMapping
-    @Cacheable("abrigos")
     fun list(
         @PageableDefault(size=10, sort=["name"], direction = Sort.Direction.ASC) pageable: Pageable
     ) = ResponseEntity.ok(service.list(pageable))
@@ -39,12 +38,10 @@ class SheltersController(private val service: ShelterService) {
     fun show(@PathVariable id: Int) = ResponseEntity.ok(service.show(id))
 
     @RequestMapping(method = [RequestMethod.PUT, RequestMethod.PATCH])
-    @Transactional
     fun update(@RequestBody @Valid request: ShelterUpdateRequest) = ResponseEntity.ok(service.update(request))
 
     @DeleteMapping("/{id}")
-    @Transactional
-    fun delete(id: Int): ResponseEntity<Unit> {
+    fun delete(@PathVariable id: Int): ResponseEntity<Unit> {
         service.delete(id)
         return ResponseEntity.noContent().build()
     }
--- src/main/kotlin/br/com/ocartaxo/kopet/api/domain/shelter/ShelterService.kt
+++ src/main/kotlin/br/com/ocartaxo/kopet/api/domain/shelter/ShelterService.kt
@@ -1,6 +1,8 @@
 package br.com.ocartaxo.kopet.api.domain.shelter
 
 import br.com.ocartaxo.kopet.api.domain.user.RegistrationService
+import jakarta.transaction.Transactional
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 
@@ -9,15 +11,19 @@ class ShelterService(
     private val shelterRepository: ShelterRepository,
     private val registrationService: RegistrationService,
 ) {
+    @Transactional
     fun register(request: ShelterRequest): ShelterSummaryResponse {
         val shelter = request.toEntity(registrationService.register(request.toRegisterRequest()))
         return shelter.toSummaryDTO()
     }
 
+    @Cacheable("abrigos")
     fun list(pageable: Pageable) = shelterRepository.findAll(pageable).map(Shelter::toSummaryDTO)
+    @Cacheable("abrigo_por_id")
     fun show(id: Int) = shelterRepository.findById(id)
         .orElseThrow { IllegalArgumentException("Abrigo de id $id não está cadastrado!") }.toDTO()
 
+    @Transactional
     fun update(request: ShelterUpdateRequest): ShelterResponse {
         val shelter = shelterRepository.findById(request.id)
             .orElseThrow { IllegalArgumentException("Abrigo não encontrado!") }
@@ -27,6 +33,7 @@ class ShelterService(
         return shelter.toDTO()
     }
 
+    @Transactional
     fun delete(id: Int) = shelterRepository.deleteById(id)
 
 
