c007a9bb6fd377c597895c18fba7a6dca9b0ca63
ocartaxo <oacartaxo@gmail.com>
2024-09-15 23:43:38 -0300

    registra usuario

--- src/main/kotlin/br/com/ocartaxo/kopet/api/domain/auth/AuthenticationService.kt
+++ src/main/kotlin/br/com/ocartaxo/kopet/api/domain/auth/AuthenticationService.kt
@@ -2,22 +2,31 @@ package br.com.ocartaxo.kopet.api.domain.auth
 
 import br.com.ocartaxo.kopet.api.domain.token.Token
 import br.com.ocartaxo.kopet.api.domain.token.TokenRepository
+import br.com.ocartaxo.kopet.api.domain.user.Role
 import br.com.ocartaxo.kopet.api.domain.user.User
+import br.com.ocartaxo.kopet.api.domain.user.UsersRepository
 import br.com.ocartaxo.kopet.api.infra.security.JwtService
 import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AuthenticationService(
     private val authenticationManager: AuthenticationManager,
-    private val jwtService: JwtService,
     private val tokenRepository: TokenRepository,
+    private val usersRepository: UsersRepository,
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtService: JwtService
+
 ) {
+
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
+    @Transactional
     fun authenticate(request: AuthenticationRequest): AuthenticationResponse {
         logger.info("I=Autenticando o usuário ${request.username}")
 
@@ -46,8 +55,26 @@ class AuthenticationService(
 
     }
 
+    @Transactional
     fun register(request: RegisterRequest): AuthenticationResponse? {
-        return null
+        logger.info("I=Cadastrando usuário ${request.username}")
+
+        val user  = User(
+            email = request.username,
+            password = passwordEncoder.encode(request.password),
+            role = request.role
+        )
+
+        usersRepository.save(user)
+
+        val jwtToken = jwtService.generateToken(user)
+        val refreshToken = jwtService.generateRefreshToken(user)
+        saveToken(user, jwtToken)
+        return AuthenticationResponse(
+            accessToken = jwtToken,
+            refreshToken = refreshToken,
+            expirationTime = jwtService.extractExpirationTime(jwtToken)
+        )
     }
 
     private fun saveToken(user: User, jwtToken: String) {
--- src/main/kotlin/br/com/ocartaxo/kopet/api/domain/auth/RegisterRequest.kt
+++ src/main/kotlin/br/com/ocartaxo/kopet/api/domain/auth/RegisterRequest.kt
@@ -1,9 +1,18 @@
 package br.com.ocartaxo.kopet.api.domain.auth
 
 import br.com.ocartaxo.kopet.api.domain.user.Role
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
 
 data class RegisterRequest(
+    @Email
+    @JsonProperty("email")
     val username: String,
+    @NotBlank
+    @JsonProperty("senha")
     var password: String,
+    @NotBlank
+    @JsonProperty("perfil")
     val role: Role
 )
