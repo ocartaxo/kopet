da68e800e4839e076958ba8503f5d00a61d560cc
ocartaxo <oacartaxo@gmail.com>
2024-09-15 22:47:26 -0300

    documentacao do endpoint de abrigo

--- src/main/kotlin/br/com/ocartaxo/kopet/api/controller/SheltersController.kt
+++ src/main/kotlin/br/com/ocartaxo/kopet/api/controller/SheltersController.kt
@@ -4,10 +4,9 @@ import br.com.ocartaxo.kopet.api.domain.shelter.ShelterRequest
 import br.com.ocartaxo.kopet.api.domain.shelter.ShelterService
 import br.com.ocartaxo.kopet.api.domain.shelter.ShelterSummaryResponse
 import br.com.ocartaxo.kopet.api.domain.shelter.ShelterUpdateRequest
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import jakarta.transaction.Transactional
 import jakarta.validation.Valid
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
@@ -17,6 +16,8 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
+@RequestMapping("/apí/abrigo", produces = [MediaType.APPLICATION_JSON_VALUE])
+@Tag(name = "Abrigos", description = "API para abrigos")
 class SheltersController(private val service: ShelterService) {
 
     @PostMapping
@@ -29,17 +30,31 @@ class SheltersController(private val service: ShelterService) {
         return ResponseEntity.created(uri).body(response)
     }
 
+    @Operation(
+        summary = "Lista informações dos abrigos",
+        description = "Lista informações sumarizadas dos abrigos cadastrados de forma paginada"
+    )
     @GetMapping
     fun list(
         @PageableDefault(size=10, sort=["name"], direction = Sort.Direction.ASC) pageable: Pageable
     ) = ResponseEntity.ok(service.list(pageable))
 
+
+    @Operation(
+        summary = "Retorna informações de um abrigo",
+        description = "Retorna informações detalhadas do abrigo de id informado"
+    )
     @GetMapping("/{id}")
     fun show(@PathVariable id: Int) = ResponseEntity.ok(service.show(id))
+
 
     @RequestMapping(method = [RequestMethod.PUT, RequestMethod.PATCH])
     fun update(@RequestBody @Valid request: ShelterUpdateRequest) = ResponseEntity.ok(service.update(request))
 
+    @Operation(
+        summary = "Remove abrigo",
+        description = "Remove o abrigo de id informado."
+    )
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Int): ResponseEntity<Unit> {
         service.delete(id)
