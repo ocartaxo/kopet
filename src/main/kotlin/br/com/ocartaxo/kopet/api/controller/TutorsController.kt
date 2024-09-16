5d7efd2bfd9cf7b2609e5db8d4250a6bf3402189
ocartaxo <oacartaxo@gmail.com>
2024-09-15 22:39:54 -0300

    descricao dos endpoints

--- src/main/kotlin/br/com/ocartaxo/kopet/api/controller/TutorsController.kt
+++ src/main/kotlin/br/com/ocartaxo/kopet/api/controller/TutorsController.kt
@@ -6,11 +6,9 @@ import br.com.ocartaxo.kopet.api.domain.tutor.TutorService
 import br.com.ocartaxo.kopet.api.domain.tutor.TutorUpdateRequest
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.data.domain.Pageable
-import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -20,7 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder
 
 @RestController
 @RequestMapping("/api/tutores", produces = [MediaType.APPLICATION_JSON_VALUE])
-@Tag(name = "Tutores", description = "API para tutores")
+@Tag(name = "Tutores", description = "CRUD para tutores")
 class TutorsController(private val service: TutorService) {
 
     @PostMapping
@@ -38,20 +36,25 @@ class TutorsController(private val service: TutorService) {
 
 
     @GetMapping
-    @Operation(summary = "Lista os tutores", security = [SecurityRequirement(name = "bearer")])
+    @Operation(
+        summary = "Lista os tutores.",
+        description = "Lista informações sumarizada dos tutores registrados de forma paginada."
+    )
     fun list(
         @PageableDefault(size=10) pageable: Pageable
     ) = ResponseEntity.ok(service.list(pageable))
 
     @GetMapping("/{id}")
-    @Operation(summary = "Exibe informações de um tutor", security = [SecurityRequirement(name = "bearer")])
+    @Operation(summary = "Exibe informações de um tutor")
     fun show(@PathVariable @Parameter(description = "id do tutor") id: Int) = service.show(id)
 
-    @Operation(description = "Atualiza informações de um tutor", security = [SecurityRequirement(name = "bearer")])
+    @Operation(
+        summary = "Atualiza as informações do tutor.",
+        description = "Atualiza informações do tutor de id informado.")
     @RequestMapping(method = [RequestMethod.PUT, RequestMethod.PATCH])
     fun update(request: TutorUpdateRequest) = service.update(request)
 
-    @Operation(description = "Remove um tutor da base de dados", security = [SecurityRequirement(name = "bearer")])
+    @Operation(description = "Remove um tutor da base de dados")
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Int): ResponseEntity<Unit> {
         service.delete(id)
